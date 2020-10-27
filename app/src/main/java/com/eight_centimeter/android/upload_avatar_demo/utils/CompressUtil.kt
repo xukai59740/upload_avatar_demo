3648fa9b968ab8c0b90164676743ab330eeb72ce
kevin.xu <kevin.xu@imaginato.com>
2020-10-27 18:13:08 +0800

    compression

--- app/src/main/java/com/eight_centimeter/android/upload_avatar_demo/utils/CompressUtil.kt
+++ app/src/main/java/com/eight_centimeter/android/upload_avatar_demo/utils/CompressUtil.kt
@@ -3,15 +3,13 @@ package com.eight_centimeter.android.upload_avatar_demo.utils
 import android.content.ContentResolver
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.provider.MediaStore
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.OutputStream
+import android.util.Log
+import java.io.*
 
 object CompressUtil {
-
 
     val MB_2: Int = 1024 * 1024 * 2
     val MB_3: Int = 1024 * 1024 * 3
@@ -39,53 +37,106 @@ object CompressUtil {
     }
 
     private fun massCompression(
-        uri: Uri,
-        contentResolver: ContentResolver,
-        destination: File,
-        quality: Int = 50
+            uri: Uri,
+            contentResolver: ContentResolver,
+            destination: File,
+            quality: Int = 50
     ) {
-        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+        val bitmap = measurementCompression(contentResolver, uri)
         var out: OutputStream? = null
         try {
             out = FileOutputStream(destination.path)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, quality, out)
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
+        } finally {
+            bitmap?.recycle()
+            out?.close()
         }
-        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, out)
+    }
+
+    fun measurementCompression(contentResolver: ContentResolver, uri: Uri): Bitmap? {
+        var input: InputStream = ImageFileUtil.getInputStreamByUri(contentResolver, uri)
+        val onlyBoundsOptions: BitmapFactory.Options = BitmapFactory.Options()
+        try {
+            onlyBoundsOptions.inJustDecodeBounds = true;
+            onlyBoundsOptions.inDither = true;
+            onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            BitmapFactory.decodeStream(input, null, onlyBoundsOptions)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        } finally {
+            input.close()
+        }
+
+        input = ImageFileUtil.getInputStreamByUri(contentResolver, uri)
+        try {
+            val originalWidth = onlyBoundsOptions.outWidth
+            val originalHeight = onlyBoundsOptions.outHeight
+            if ((originalWidth == -1) || (originalHeight == -1)) return null
+
+            Log.d("kevins", "originalHeight ${originalHeight}")
+            Log.d("kevins", "originalHeight originalWidth ${originalWidth}")
+            //图片分辨率以480x800为标准
+            var hh = 1080f
+            var ww = 1080f
+            //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+            var be = 1 //be=1表示不缩放
+            if (originalWidth > originalHeight && originalWidth > ww) {//如果宽度大的话根据宽度固定大小缩放
+                be = (originalWidth / ww).toInt()
+            } else if (originalWidth < originalHeight && originalHeight > hh) {//如果高度高的话根据宽度固定大小缩放
+                be = (originalHeight / hh).toInt()
+            }
+            if (be <= 0) be = 1
+
+            //比例压缩
+            var bitmapOptions = BitmapFactory.Options()
+            bitmapOptions.inSampleSize = be
+            bitmapOptions.inDither = true
+            bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888
+
+            return BitmapFactory.decodeStream(input, null, bitmapOptions)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        } finally {
+            input.close()
+        }
     }
 
     /**
      * check the max size, and compress it
      */
     private fun getQuality(file: File): Int {
-        val mb :Float = file.length().toFloat() / 1024 / 1024
+        val mb: Float = file.length().toFloat() / 1024 / 1024
         return when {
             mb > 8 -> {
-                10
-            }
-            mb > 7 -> {
-                20
-            }
-            mb > 6 -> {
                 30
             }
-            mb > 4.5 -> {
+            mb > 7 -> {
                 40
             }
-            mb > 3.5 -> {
+            mb > 6 -> {
+                45
+            }
+            mb > 4.5 -> {
                 50
             }
-            mb > 2.5 -> {
+            mb > 3.5 -> {
                 60
             }
-            mb > 1.5 -> {
-                65
-            }
-            mb > 1 -> {
+            mb > 2.5 -> {
                 70
             }
-            else -> {
+            mb > 1.5 -> {
                 75
+            }
+            mb > 1 -> {
+                80
+            }
+            else -> {
+                85
             }
         }
     }
--- app/src/main/java/com/eight_centimeter/android/upload_avatar_demo/utils/ImageFileUtil.kt
+++ app/src/main/java/com/eight_centimeter/android/upload_avatar_demo/utils/ImageFileUtil.kt
@@ -1,121 +1,59 @@
 package com.eight_centimeter.android.upload_avatar_demo.utils
 
-import android.content.ContentValues
+import android.content.ContentResolver
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.net.Uri
-import android.os.Build
 import android.os.Environment
-import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.exifinterface.media.ExifInterface
-import com.bumptech.glide.Glide
 import com.eight_centimeter.android.upload_avatar_demo.BuildConfig
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import org.apache.commons.io.IOUtils
 import java.io.*
 import java.util.*
 
-
 object ImageFileUtil {
-
-    fun saveUrlInMedia(context: Context, url: String): Disposable {
-        return Single.just(Unit)
-                .subscribeOn(Schedulers.io())
-                .flatMap {
-                    val file = Glide.with(context).downloadOnly().load(url).submit().get()
-                    Single.just(file)
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    saveInMedia(context, it)
-                }, {
-
-                })
-    }
-
-    private fun saveInMedia(context: Context, file: File): Uri? {
-        // insert media
-        val contentUri = if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        } else MediaStore.Images.Media.INTERNAL_CONTENT_URI
-
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, getTimeFileName())
-        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/*")
-        contentValues.put(MediaStore.Images.Media.DATE_ADDED, Date().time)
-        contentValues.put(MediaStore.Images.Media.DATE_TAKEN, Date().time)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            contentValues.put(
-                    MediaStore.Images.Media.RELATIVE_PATH,
-                    Environment.DIRECTORY_DCIM + File.separator
-            )
-            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 1)
-        }
-        val uri = context.contentResolver.insert(contentUri, contentValues) ?: return null
-
-        //save
-        var os: OutputStream? = null
-        return try {
-            os = context.contentResolver.openOutputStream(uri)
-            IOUtils.copy(FileInputStream(file), os!!)
-            contentValues.clear()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
-                context.contentResolver.update(uri, contentValues, null, null)
-            }
-            uri
-        } catch (e: java.lang.Exception) {
-            e.printStackTrace()
-            // 失败的时候，删除此 uri 记录
-            context.contentResolver.delete(uri, null, null)
-            null
-        } finally {
-            try {
-                os?.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-    }
 
     /**
      * if samsung take photo , the image be Rotate 90°， fix it
      */
-    fun fixBitmapRotate(context: Context, uri: Uri): Uri {
+    fun fixBitmapRotate(context: Context, uri: Uri, quality: Int = 100): Uri {
         var out: OutputStream? = null
+        var bitmap: Bitmap? = null
+        var returnBm: Bitmap? = null
         try {
             val degree = getBitmapDegree(context, uri)
             if (degree != 0) {
                 val matrix = Matrix()
                 matrix.setRotate(degree.toFloat())
-                //android:largeHeap="true"
-                val bitmap: Bitmap =
-                        MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
-                val returnBm =
-                        Bitmap.createBitmap(
-                                bitmap,
-                                0,
-                                0,
-                                bitmap.width,
-                                bitmap.height,
-                                matrix,
-                                true
-                        )
-                val destFile = createNewJPGFile(context)
 
-                out = FileOutputStream(destFile.path)
-                returnBm.compress(Bitmap.CompressFormat.JPEG, 100, out)
-                return destFile.toUri()
+                // compress measurementCompression bitmap
+                bitmap = CompressUtil.measurementCompression(context.contentResolver, uri)
+
+                if (bitmap != null) {
+                    returnBm = Bitmap.createBitmap(
+                            bitmap,
+                            0,
+                            0,
+                            bitmap.width,
+                            bitmap.height,
+                            matrix,
+                            true
+                    )
+                    val destFile = createNewJPGFile(context)
+                    out = FileOutputStream(destFile.path)
+                    returnBm.compress(Bitmap.CompressFormat.JPEG, quality, out)
+                    return destFile.toUri()
+                }
             }
         } catch (e: Exception) {
             e.printStackTrace()
+        } finally {
             out?.close()
+            returnBm?.recycle()
+            bitmap?.recycle()
         }
         return uri
     }
@@ -146,7 +84,7 @@ object ImageFileUtil {
     }
 
     fun createNewJPGFile(context: Context): File {
-        ///sdcard/Android/data/application/dcim/images/123123123.jpg
+        ///sdcard/Android/data/asia.mworks.impulso.dev/cache/images/123123123.jpg
         val folder = File(context.getExternalFilesDir(Environment.DIRECTORY_DCIM).toString())
         folder.mkdirs()
         val file = File(folder, getTimeFileName())
@@ -172,7 +110,7 @@ object ImageFileUtil {
     }
 
     private fun convertFileToUriByProvider(context: Context, file: File): Uri {
-        //content:///application.fileprovider/external_file_dcim/123123123.jpg
+        //content:///asia.mworks.impulso.dev.fileprovider/external_file_dcim/123123123.jpg
         val imageUri = FileProvider.getUriForFile(
                 context,
                 BuildConfig.APPLICATION_ID + ".fileprovider",
@@ -182,13 +120,26 @@ object ImageFileUtil {
     }
 
     fun copyFileFromFileDescriptor(context: Context, uri: Uri): File {
-        val parcelFileDescriptor = context.contentResolver.openFileDescriptor(uri, "r", null)
-        val inputStream = FileInputStream(parcelFileDescriptor?.fileDescriptor)
-        val outFile = createNewJPGFile(context)
-        val outputStream = FileOutputStream(outFile)
-        IOUtils.copy(inputStream, outputStream)
-        return outFile
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+        try {
+            inputStream = getInputStreamByUri(context.contentResolver, uri)
+            val outFile = createNewJPGFile(context)
+            outputStream = FileOutputStream(outFile)
+            IOUtils.copy(inputStream, outputStream)
+            return outFile
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return createNewJPGFile(context)
+        } finally {
+            inputStream?.close()
+            outputStream?.close()
+        }
     }
 
+    fun getInputStreamByUri(contentResolver: ContentResolver, uri: Uri): InputStream {
+        val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r", null)
+        return FileInputStream(parcelFileDescriptor?.fileDescriptor)
+    }
 
 }
